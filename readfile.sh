1cf3c9ee0ebcdb58180937867b5eda81b6006f96
Shai25 <shaiyneeman@gmail.com>
2025-03-20 13:34:11 +0200

    adding read file script

---
+++ readfile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#ask for a file, check if exists, if yes - return words, lines and total file size:
+read -p "Choose a file " cfi
+che=$(ls -l | grep "$cfi")
+if [ -n "$che" ];then
+	echo "the file content is: "
+	cat $cfi
+	wcf=$( cat $cfi | wc )
+	echo "Total lines, words and chars: $wcf "
+	echo "File Size: $filesize"
+	filesize= du -sh | awk '{print $1}'
+
+#search function:
+
+	read -p "Enter a word to search in the file " sea
+	if [[ -n $(cat $cfi | grep $sea) ]];then
+		pos=$(tr -s ' ' '\n' < $cfi | grep -n $sea | cut -d ':' -f1)
+		echo "True, Word:$equ is number $pos in the file"
+	else
+       		echo "No such word in this file "
+	fi
+else
+	echo "No such file in this directory "
+fi
