6e88281ac5e3f8329e01df3ce9b3ede49e83f3ac
Yehuda-Dayan <dyehuda10@gmail.com>
2025-03-22 21:14:31 +0000

    Fannan

--- Groop_Manager.sh
+++ Groop_Manager.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 # call for utility
 echo "how can i help you today?"
-read -p "if you want use the utility tool pleas enter 1, if you want to use the read tool please enter 2, if you want to use the delete tool please enter 3. " x
-if [[ $x -lt 1 || $x -gt 3 ]]; then
-	echo "please enter a propper answer"
-	exit 0
-elif [[ $x -eq 1 ]]; then
-	echo "great you choose utility"
-elif [[ $x -eq 2 ]]; then
-	echo "2"
-elif [[ $x -eq 3 ]]; then
-	echo "3"
-fi
-
-
+PS3="choos utility, read or delet"
+option=("utility" "read" "delet")
+select opt in "${option[@]}"; do
+	case $REPLY in
+		utility)
+			echo "you chose utility"
+			./zivs_script_utility.sh
+			break
+			;;
+		read)
+			echo "you chose read"
+			./readfile.sh
+			break
+			;;
+		delet)
+			echo "you chose delet"
+			./delet.sh
+			break
+			;;
+		*)
+			echo "ilegal choice"
+			exit 0
+			;;
+	esac
+done
